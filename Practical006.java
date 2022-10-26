7d4fa16cd1df1536a9b0d566ab6b149109c5c40d
Artem <xTema05x@mail.ru>
2022-10-26 20:32:51 +0300

    Волновой алгоритм(не закончено)

--- Practical006.java
+++ Practical006.java
@@ -8,7 +8,7 @@ public class Practical006 {
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
-                array[i][j] = 1; // заполнение массива еденицами
+                array[i][j] = 1; // заполнение массива единицами 
                 array[1][1] = 0; // start
                 array[3][3] = 0; // end
                 System.out.print(array[i][j] + "  ");
