7cd255faa0e4874d48081cca7ae122761b90b893
Artem <xTema05x@mail.ru>
2022-10-26 20:26:44 +0300

    Волновой алгоритм(не закончено)

---
+++ Practical006.java
@@ -0,0 +1,19 @@
+// Волновой алгоритм
+
+public class Practical006 {
+
+    public static void main(String[] args) {
+        int[][] array = new int[5][5];
+        int result = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                array[i][j] = 1; // заполнение массива еденицами
+                array[1][1] = 0; // start
+                array[3][3] = 0; // end
+                System.out.print(array[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
