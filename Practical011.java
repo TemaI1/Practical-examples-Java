d8370a548492830db664ed3ac72bb50bef7c63b6
Artem <xTema05x@mail.ru>
2022-11-02 17:39:39 +0300

    switch

---
+++ Practical011.java
@@ -0,0 +1,18 @@
+// switch
+
+public class Practical011 {
+    public static void main(String[] args) {
+        int age = 18;
+        switch (age) {
+            case 23:
+                System.out.println("Тебе 23 года");
+                break;
+            case 18:
+                System.out.println("Тебе 18 лет");
+                break;
+            default:
+                System.out.println("Ничего не вышло");
+                break;
+        }
+    }
+}
