b46ccf99f287d5dfaa1a180cbde167d31431f431
Artem <xTema05x@mail.ru>
2022-11-06 18:57:01 +0300

    array

---
+++ Practical012.java
@@ -0,0 +1,42 @@
+// array
+
+public class Practical012 {
+    public static void main(String[] args) {
+        // int[] num = new int[5];
+        // for(int i = 0; i < num.length; i++){
+        //     num[i] = i * 10;
+        //     System.out.print(num[i] + " ");
+        // }
+
+        // String[] str = new String[5];
+        // for (String string : str) {
+        //     string = "hello";
+        //     System.out.print(string + " ");
+        // }
+
+        // int[] num = {8, 3, 7, 4};
+        // int sum = 0;
+        // for (int i : num) {
+        //     sum += i;
+        // }
+        // System.out.println(sum);
+
+        // String[][] array = new String[3][3];
+        // for (int i = 0; i < array.length; i++) {
+        //     for (int j = 0; j < array.length; j++) {
+        //         array[i][j] = "hello";
+        //         System.out.print(array[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // int[][] array = {{2, 3, 4, 1}, {6, 1, 5, 7, 3}, {9, 4, 7}};
+        // for (int i = 0; i < array.length; i++) {
+        //     for (int j = 0; j < array[i].length; j++) {
+        //         System.out.print(array[i][j] + " ");
+        // }
+        //     System.out.println();
+        // }
+
+    }
+}
