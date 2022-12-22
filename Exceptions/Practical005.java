13277dadfdbe028f3b8791e9a27e0ab5066f3aea
Artem <xTema05x@mail.ru>
2022-12-22 14:03:45 +0300

    Проверка строки

---
+++ Exceptions/Practical005.java
@@ -0,0 +1,26 @@
+// Проверка строки
+
+package Exceptions;
+
+import java.util.Scanner;
+
+public class Practical005 {
+
+    public static void main(String[] args) throws Exception{
+        System.out.println("Введите любое значение: ");
+        Scanner scan = new Scanner(System.in);
+        String result = (scan.nextLine());
+
+        if (result == null || result.isEmpty() || result.equals(" ")) {
+            try {
+                throw new Exception("Ваша строка пуста!");
+            } catch (Exception e) {
+                System.out.println("Ваша строка пуста!");
+            }
+        }
+        else{
+            System.out.println("Ваша строка: " + result);
+        }
+            
+    }
+}
